17c96da6c53bb44c66780131f3b31cd89a54ea7e
Vini3x3 <vincent.hanghang@gmail.com>
2024-06-29 16:13:58 +0800

    THUB-03: add query api

--- src/main/java/com/transactionHub/repository/TransactionRepository.java
+++ src/main/java/com/transactionHub/repository/TransactionRepository.java
@@ -3,17 +3,26 @@ package com.transactionHub.repository;
 import com.transactionHub.entity.Transaction;
 import com.transactionHub.transactionCoreLibrary.constant.AccountEnum;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import io.quarkus.panache.common.Sort;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @ApplicationScoped
 public class TransactionRepository implements PanacheMongoRepository<Transaction> {
 
-    public List<Transaction> findTransactionByDate(Date start, Date end, AccountEnum account){
-        return find("_id.date >= ?1 and _id.date <= ?2 and _id.account = ?3", start, end, account).stream().toList();
-    }
+    public List<Transaction> findTransactionsByDate(Date start, Date end, AccountEnum account) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("startDate", start);
+        params.put("endDate", end);
+        params.put("account", account);
 
+        String query = "{ $and: [ { '_id.date' : { $gte: :startDate } }, { '_id.date' : { $lte: :endDate } }, { '_id.account' : :account } ] }";
+
+        return list(query, params);
+    }
 }
---
+++ src/main/java/com/transactionHub/service/QueryService.java
@@ -0,0 +1,26 @@
+package com.transactionHub.service;
+
+import com.transactionHub.entity.TransactionTranslator;
+import com.transactionHub.repository.TransactionRepository;
+import com.transactionHub.transactionCoreLibrary.constant.AccountEnum;
+import com.transactionHub.transactionCoreLibrary.domain.Transaction;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.util.Date;
+import java.util.List;
+
+@ApplicationScoped
+public class QueryService {
+
+    @Inject
+    TransactionRepository repository;
+
+    public List<Transaction> findTransactionsByDate(Date start, Date end, AccountEnum account) {
+        return repository.findTransactionsByDate(start, end, account)
+                .stream()
+                .map(TransactionTranslator::mapToDomain)
+                .sorted()
+                .toList();
+    }
+}
