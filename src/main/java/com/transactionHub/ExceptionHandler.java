32f82e58c5d47ffb13db48033f02445ff35b763c
Vini3x3 <vincent.hanghang@gmail.com>
2024-07-03 23:12:47 +0800

    THUB-06: improve ExceptionHandler

--- src/main/java/com/transactionHub/ExceptionHandler.java
+++ src/main/java/com/transactionHub/ExceptionHandler.java
@@ -4,11 +4,16 @@ import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 
-public class ExceptionHandler  implements ExceptionMapper<Exception> {
+public class ExceptionHandler implements ExceptionMapper<Exception> {
     @Override
     public Response toResponse(Exception e) {
         if (e instanceof NotFoundException) {
             return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        if (e instanceof IllegalArgumentException illegalArgumentException) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(illegalArgumentException.getMessage())
+                    .build();
         }
         return null;
     }
